09b34f71d756490fd2ee952fc8ac087c001cc42f
AntoniEspinal <97680717+AntoniEspinal@users.noreply.github.com>
2023-05-30 12:28:43 -0400

    ok

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -9,11 +9,13 @@ public class PlayerController : MonoBehaviour
     public float speed = 5;
 
     public SpriteRenderer sprite;
+
+    private Animator anim;
     
     // Start is called before the first frame update
     void Start()
     {
-       
+       anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,13 +23,19 @@ public class PlayerController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         transform.Translate(Vector2.right * speed * Time.deltaTime * horizontal);
-        if(horizontal == 1)
+        if(horizontal > 0)
         {
             sprite.flipX = true;
+            anim.SetFloat("run", true);
         }
-        else
+        else if(horizontal < 0)
         {
             sprite.flipX = false;
+            anim.SetFloat("run", true);
+        }
+        else if(horizontal == 0)
+        {
+            
         }
     }
 }
